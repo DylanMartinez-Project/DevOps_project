455ea64411b3bab0ed0ca7f64bfd21b702aa8966
dylan <dylan@dylan-530U3BI-530U4BI-530U4BH>
2022-01-29 18:08:44 -0500

    "Initial Commit and repo creation"

---
+++ src/HelloDylan.java
@@ -0,0 +1,15 @@
+
+public class HelloDylan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Dylan!");
+		System.out.println("Test project to build CI/CD pipeline using devOps tools");
+		
+
+	}
+	
+
+
+}
